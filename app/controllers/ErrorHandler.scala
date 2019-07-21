42a270e461a223074003825e2fc0341455cfc3fe
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-21 08:29:23 +0100

    Error Handler Added

---
+++ app/controllers/ErrorHandler.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+
+
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful(
+      Redirect("/")
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+
+
+
+
+
+}
