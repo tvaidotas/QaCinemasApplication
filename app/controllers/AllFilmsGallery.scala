3e9f93eb029a111baa6a9fdf5da20bbcb11c34bf
Joshua <joshuagomersallgebba@gmail.com>
2019-07-22 10:50:03 +0100

    PlaceHolder Database Made Into Helper Object

--- app/controllers/AllFilmsGallery.scala
+++ app/controllers/AllFilmsGallery.scala
@@ -1,14 +1,13 @@
 package controllers
 
+import controllers.placeholderDatabase
+
 import play.api._
 import play.api.mvc._
 
 class AllFilmsGallery extends Controller {
-
-  val allFilmsList :List[String] = List[String]("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18")
-
   def allFilms = Action {
-    Ok(views.html.allFilmsGallery(allFilmsList))
+    Ok(views.html.allFilmsGallery(placeholderDatabase.allFilmsList))
   }
 
 }
--- app/controllers/NewRelesesGallery.scala
+++ app/controllers/NewRelesesGallery.scala
@@ -5,10 +5,8 @@ import play.api.mvc._
 
 class NewRelesesGallery extends Controller {
 
-  val allFilmsList :List[String] = List[String]("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18")
-
   def newReleses = Action {
-    Ok(views.html.newRelesesGallery(allFilmsList))
+    Ok(views.html.newRelesesGallery(placeholderDatabase.allFilmsList))
   }
 
 }
