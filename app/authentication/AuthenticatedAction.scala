db704d8558d64d7188c03838f89a9357facd25b3
uno1bruno2 <bruno.sterner@qa.com>
2019-07-22 16:01:33 +0100

    Implemented Authenticated Action. Removed CSS on Login Page

--- app/authentication/AuthenticatedAction.scala
+++ app/authentication/AuthenticatedAction.scala
@@ -11,9 +11,9 @@ object AuthenticatedAction extends ActionBuilder[AuthenticatedRequest] {
   def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]): Future[Result] = {
 
     request.session.get("name")
-      .flatMap(user => Users.checkUser(user)) // get the optional user if the name is valid
-      .map(user => block(new AuthenticatedRequest[A](user.uName,request))) // if we found the user we're all good
-      .getOrElse(Future.successful(Redirect("/login"))) // we haven't found the user
+      .flatMap(user => Users.checkUser(user))
+      .map(user => block(new AuthenticatedRequest[A](user.uName,request)))
+      .getOrElse(Future.successful(Redirect("/login")))
   }
 
 }
