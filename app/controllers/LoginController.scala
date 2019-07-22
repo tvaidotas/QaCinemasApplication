d6baafbf16cc334ddbee6b55bc27cdd5819852af
matt712 <mzgadd@gmail.com>
2019-07-22 11:00:58 +0100

    added a basic login functionality to be expanded later

---
+++ app/controllers/LoginController.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import models.Users
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+
+class LoginController @Inject()(val messagesApi: MessagesApi, environment: play.api.Environment) extends Controller with I18nSupport {
+
+  def logIn() = Action {implicit request =>
+    val loginForm = Users.loginForm.bindFromRequest
+    loginForm.fold({ formWithErrors =>
+      BadRequest(views.html.login(formWithErrors))
+    }, { login =>
+      if(Users.validUsers.contains(login)) {
+        Redirect("/home").withSession("login" -> "true")
+      }
+      else{
+        Redirect("/")
+      }
+    })
+  }
+}
