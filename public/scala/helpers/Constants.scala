cae537bbe72ca7a4048789e994122965794ce0e4
VLukovski <valentinlukovski1@gmail.com>
2019-07-22 11:49:04 +0100

    removed mongodb files that carried over by accident

--- public/scala/helpers/Constants.scala
+++
@@ -1,9 +0,0 @@
-package helpers
-
-object Constants extends Enumeration {
-
-  val localHost: Value = Value("localhost")
-  val insertedDocument: Value = Value("inserted document: ")
-  val removedDocument: Value = Value("removed document: ")
-
-}
