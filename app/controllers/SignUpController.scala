072cf24906357c6e77e27c6c7b55d59ae7119271
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-22 21:34:28 +0100

    Extra SignUp Code Removed , now checks all valid users when you sign up

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -1,18 +1,19 @@
 package controllers
 
 import javax.inject.Inject
-import models.UsersSignUp
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import models.UsersSignUp
+import models.Users
 
 class SignUpController @Inject()(val messagesApi: MessagesApi, environment: play.api.Environment) extends Controller with I18nSupport {
 
   def signUp() = Action {implicit request =>
     UsersSignUp.signUpForm.bindFromRequest.fold({ formWithErrors =>
       BadRequest(views.html.signUp(formWithErrors))
-    }, { signUp =>
-      if(UsersSignUp.validUsers.contains(signUp)) {
-        Redirect("/").withSession("name" -> s"${signUp.uName}")
+    }, { signup =>
+      if(Users.validUsers.contains(signup)) {
+        Redirect("/").withSession("name" -> s"${signup.uName}")
       }
       else{
         //TODO Add user to DB and login or redirect to login
--- app/models/UsersSignUp.scala
+++ app/models/UsersSignUp.scala
@@ -3,7 +3,6 @@ package models
 import play.api.data.Form
 import play.api.data.Forms.mapping
 import play.api.data.Forms._
-import scala.collection.mutable.ArrayBuffer
 
 case class UsersSignUp(uName: String, pWord: String ,postCode: String,houseNo: String)
 object UsersSignUp{
@@ -15,13 +14,4 @@ object UsersSignUp{
       "house_no" -> nonEmptyText
     )(UsersSignUp.apply)(UsersSignUp.unapply)
   )
-
-  val validUsers = ArrayBuffer[Users](
-    Users("admin", "admin"),
-    Users("pleb", "plebian")
-  )
-
-  def checkUser(username: String) = {
-    Some(validUsers.filter(user => user.uName == username).head)
-  }
 }
