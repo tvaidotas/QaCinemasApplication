781334e5cf97e540c76abc8ca6aa58bb4e63a26b
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-24 14:45:47 +0100

    added default results to allfilms and new releases pages

--- app/controllers/FilmDetails.scala
+++ app/controllers/FilmDetails.scala
@@ -6,7 +6,7 @@ import play.api.mvc.{Action, Controller}
 class FilmDetails extends Controller {
   def filmDetails(id: String) = Action {
     Ok(views.html.filmDetails(PlaceholderDatabase.allFilmsList.find(_.getId() == id.toInt).getOrElse(new Film(1,"error",
-      "error", "error", "error", "12-06-2000", List("Arnold", "John", "James")))))
+      "error", "error", "error", "00-00-0000", List("error")))))
   }
 
 }
