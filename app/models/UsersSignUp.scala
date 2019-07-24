e96107b11f072c8f3f16fbe99229c31a6cd87a22
Joshua <joshuagomersallgebba@gmail.com>
2019-07-24 09:58:56 +0100

    SignUp - first and surname added

--- app/models/UsersSignUp.scala
+++ app/models/UsersSignUp.scala
@@ -7,10 +7,12 @@ import play.api.data.Forms._
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
-case class UsersSignUp(uName: String, pWord: String ,postCode: String,houseNo: String)
+case class UsersSignUp(fName: String, lName: String,uName: String, pWord: String ,postCode: String,houseNo: String)
 object UsersSignUp{
   val signUpForm = Form (
     mapping(
+      "fName" -> nonEmptyText,
+      "lName" -> nonEmptyText,
       "name" -> nonEmptyText,
       "password" -> nonEmptyText,
       "postcode" -> nonEmptyText,
