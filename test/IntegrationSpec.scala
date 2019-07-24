3561e3f2e3065140ed301b39f2483391ccf896aa
Joshua <joshuagomersallgebba@gmail.com>
2019-07-24 14:24:36 +0100

    NavBar Updated to match wireframes

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -13,11 +13,8 @@ import play.api.test.Helpers._
 class IntegrationSpec extends Specification {
 
   "Application" should {
-
     "work from within a browser" in new WithBrowser {
-
       browser.goTo("http://localhost:" + port)
-
       browser.pageSource must contain("Your new application is ready.")
     }
   }
