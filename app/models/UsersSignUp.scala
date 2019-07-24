59c0da09c6706456335a8c550cb21ec16e5332a6
Joshua <joshuagomersallgebba@gmail.com>
2019-07-24 10:14:00 +0100

    labels updated to black , additional sign up fields added

--- app/models/UsersSignUp.scala
+++ app/models/UsersSignUp.scala
@@ -7,16 +7,17 @@ import play.api.data.Forms._
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
-case class UsersSignUp(fName: String, lName: String,uName: String, pWord: String ,postCode: String,houseNo: String)
+case class UsersSignUp(fName: String, lName: String,uName: String, email: String , pWord: String ,postCode: String,houseNo: String)
 object UsersSignUp{
   val signUpForm = Form (
     mapping(
       "fName" -> nonEmptyText,
       "lName" -> nonEmptyText,
-      "name" -> nonEmptyText,
+      "uName" -> nonEmptyText,
+      "email" -> nonEmptyText,
       "password" -> nonEmptyText,
       "postcode" -> nonEmptyText,
-      "house_no" -> nonEmptyText
+      "address" -> nonEmptyText
     )(UsersSignUp.apply)(UsersSignUp.unapply)
   )
 }
